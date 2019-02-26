6507f1a3573e9575efa5ddd6b4fe12fcd0d81d99
Nick O'Neill <nick.oneill@gmail.com>
2019-02-26 10:50:09 -0800

    Basic scraper for secretaries of state.

--- scraper.js
+++ scraper.js
@@ -8,22 +8,23 @@ function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT, title TEXT, state TEXT, phone TEXT, photoURL TEXT)");
 		callback(db);
 	});
 }
 
-function updateRow(db, value) {
+function updateRow(db, name, title, state, phone, photoURL) {
 	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
+	var statement = db.prepare("INSERT INTO data VALUES (?, ?, ?, ?, ?)");
+	statement.run(name, title, state, phone, photoURL);
 	statement.finalize();
 }
 
 function readRows(db) {
 	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
+	db.each("SELECT * FROM data", function(err, row) {
+		// 	"AL": {Name: "John Merrill", Phone: "334-242-7200", Title: "Secretary of State", PhotoURL: "http://www.nass.org/images/stories/merrill-al.jpg"},
+		console.log("\"" + row.state + "\": {Name: \"" + row.name + "\", Phone: \""+row.phone+"\", Title: \""+row.title+"\", PhotoURL: \""+row.photoURL+"\"},");
 	});
 }
 
@@ -41,13 +42,18 @@ function fetchPage(url, callback) {
 
 function run(db) {
 	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
+	fetchPage("https://www.nass.org/membership", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
+		var elements = $("section.sos .view-content>article").each(function () {
+			var state = $(this).find("h2").text().trim();
+			var name = $(this).children().eq(2).text().trim();
+			var photo = $(this).find("img").attr('src');
+			var title = $(this).children().eq(3).text().trim();
+			var phone = $(this).children().eq(5).text().trim();
+
+			updateRow(db, name, title, state, phone, photo);
 		});
 
 		readRows(db);
